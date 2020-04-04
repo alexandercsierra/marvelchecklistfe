cd7826e710d20e80e7a8a66b82e5dcb33996571a
Jakeas <terpjakeas@gmail.com>
2020-04-04 15:23:48 -0400

    Added nav bar

--- src/utils.js
+++ src/utils.js
@@ -40,31 +40,8 @@
     xhr.send(jsonStr);
   }
 
-//   export function getMovies(movies, currentMovie){
-//     console.log('inside get movies')
-//     var xhr = new XMLHttpRequest();
-//     xhr.onload = function () {
-    
-//         if (xhr.status >= 200 && xhr.status < 300) {
-//             console.log('success!');
-//             let parsed = JSON.parse(xhr.response);
-//             movies = parsed;
-//             appendMovies(movies);
-//             document.querySelectorAll(".card").forEach(card => {
-//             card.addEventListener("click", e => {
-//                 currentMovie = e.path[2].innerText;
-//                 console.log("currentMovie", currentMovie);
-//             });
-//             });
-//         } else {
-//             console.log('The request failed!', xhr);
-//         }
-//     };
-    
-//     let token = localStorage.getItem('token')
-//     xhr.open('GET', 'https://marvelchecklist.herokuapp.com/api/movies');
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     xhr.setRequestHeader("Authorization", token);
-//     xhr.send();
-//   }
-
+  const signOut = document.querySelector(".signOut");
+  signOut.addEventListener("click", e =>{
+    localStorage.clear();
+    window.location.href = "../index.html"
+  } )
