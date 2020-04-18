e4e38311012fd5c4401e1aaa242e585bc07da4e6
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-04-18 17:10:59 -0400

    added responsiveness

--- src/login.js
+++ src/login.js
@@ -3,6 +3,11 @@ import {login} from './utils.js';
 const username = document.querySelector("#username");
 const password = document.querySelector("#password");
 const loginForm = document.querySelector(".loginForm");
+const signUpBtn = document.querySelector(".signUpBtn");
+
+signUpBtn.addEventListener('click', ()=>{
+  window.location.href="./signup.html"
+})
 
 loginForm.addEventListener("submit", e => {
   e.preventDefault();
